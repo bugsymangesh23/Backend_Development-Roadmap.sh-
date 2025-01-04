2ecb1ad068f13c696532384efdbcfb17251660e3
bugsymangesh23 <emmanuelmangera08@gmail.com>
2025-01-04 23:19:48 +0300

    home view redirect

--- To-do_app/Todolist/TodoApp/views.py
+++ To-do_app/Todolist/TodoApp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+#class based views
+def home(request):
+    return HttpResponse("Working fine!")
