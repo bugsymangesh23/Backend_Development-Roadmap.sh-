7875e45b9044d23ef14e4d5e3a61980488a7db10
bugsymangesh23 <emmanuelmangera08@gmail.com>
2025-01-09 22:30:17 +0300

    Feat:bind socket to server

--- Python_web-server/main.py
+++ Python_web-server/main.py
@@ -4,3 +4,6 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #UDP protocol (
 
 #Settings to change behaviour of the socket
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Binding socket to server(ip adress and port)
+server_socket.bind((0.0.0.0))
