606eeb429806ce07ea1eb34495bc1d5731e61612
bugsymangesh23 <emmanuelmangera08@gmail.com>
2025-01-10 23:22:51 +0300

    Error handling

--- Python_web-server/main.py
+++ Python_web-server/main.py
@@ -1,10 +1,11 @@
 # Socket initialization
 import socket
+import time
 
 HOST_SERVER = "0.0.0.0"
 HOST_PORT = 8080
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #UDP protocol (SOCK_DGRAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #UDP protocol (SOCK_DGRAM)
 
 #Settings to change behaviour of the socket
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -14,9 +15,14 @@ server_socket.bind((HOST_SERVER, HOST_PORT)) # Acess from any machine
 
 # Listening to requests
 server_socket.listen(5)# maximum number of connections on queue
+
 print(f"Listening on port {HOST_SERVER}...")
 
+try:
 # Accepting connection requets(blocking and non-blocking)
-client_socket, client_address = server_socket.accept()
-print(client_address)
-print(client_socket)
+    client_socket, client_address = server_socket.accept()
+    print(client_address)
+    print(client_socket)
+except:
+    time.sleep(1)
+    print("Error loading page")
