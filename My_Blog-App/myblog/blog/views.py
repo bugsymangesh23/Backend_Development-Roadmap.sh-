1d30894c34865e4cb9e31546484813fe1dcc5774
bugsymangesh23 <emmanuelmangera08@gmail.com>
2025-01-18 23:53:09 +0300

    handled the views

--- My_Blog-App/myblog/blog/views.py
+++ My_Blog-App/myblog/blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object+or_404
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all() #fetch all post form database
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk) #fetch post by primary key(pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
